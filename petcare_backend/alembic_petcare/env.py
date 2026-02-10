826b60d7d08c5a2ff4692af3400c3b289a148397
behnam <behnam@behnams-MacBook-Pro.local>
2026-02-10 13:48:14 -0800

    first commit

--- petcare_backend/alembic_petcare/env.py
+++ petcare_backend/alembic_petcare/env.py
@@ -28,6 +28,16 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
+def do_run_migrations(connection) -> None:
+    context.configure(
+        connection=connection,
+        target_metadata=target_metadata,
+        compare_type=True,
+    )
+    with context.begin_transaction():
+        context.run_migrations()
+
+
 async def run_migrations_online() -> None:
     configuration = config.get_section(config.config_ini_section, {})
     configuration["sqlalchemy.url"] = settings.database_url
@@ -38,16 +48,7 @@ async def run_migrations_online() -> None:
     )
 
     async with connectable.connect() as connection:
-        await connection.run_sync(
-            lambda sync_connection: context.configure(
-                connection=sync_connection,
-                target_metadata=target_metadata,
-                compare_type=True,
-            )
-        )
-
-        with context.begin_transaction():
-            context.run_migrations()
+        await connection.run_sync(do_run_migrations)
 
     await connectable.dispose()
 
