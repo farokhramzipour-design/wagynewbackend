ab32ba409bfcc6160b6d90a060f2bfbe5c565b6f
behnam <behnam@behnams-MacBook-Pro.local>
2026-02-10 13:53:46 -0800

    first commit

--- petcare_backend/app/core/database.py
+++ petcare_backend/app/core/database.py
@@ -8,7 +8,7 @@ engine = create_async_engine(
     settings.database_url,
     pool_pre_ping=True,
     poolclass=NullPool,
-    statement_cache_size=1000,
+    connect_args={"statement_cache_size": 1000},
 )
 
 AsyncSessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
